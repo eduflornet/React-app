602989816826d82eba92d7055254a1acc01d61c8
Eduardo Flores <@eduflornet>
2021-06-27 17:16:30 +0200

    Added images and house component

--- globomantics/src/main-page/index.js
+++ globomantics/src/main-page/index.js
@@ -1,34 +1,68 @@
-import { useState, useEffect } from "react";
-import logo from './logo.svg';
+import {
+  Component
+} from "react";
 import './main-page.css';
 import Header from './header';
+import FeatureHouse from './featured-house';
+import HouseFilter from './house-filter';
+class App extends Component {
 
+  // state = {} // another way to initialize the state
 
-function App() {
-  const [allHouses, setAllHouses] = useState([]);
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
 
-  useEffect(() => {
-    const fetchHouses = async () => {
-      const rsp = await fetch("/houses.json");
-      const houses = await rsp.json();
-      setAllHouses(houses);
+  componentDidMount(){
+    this.fetchHouses();
+  }
+
+  componentDidCatch(error, info){
+    this.setState({hasError: true});
+    console.error(error, info);
+  }
+
+  fetchHouses = () => {
+    fetch('/houses.json')
+    .then(rsp => rsp.json())
+    .then(allHouses => {
+      this.allHouses = allHouses;
+      this.determineFeaturedHouse();
+      this.determineUniqueConstries();
+    })
+  }
+
+  determineFeaturedHouse = () => {
+    if (this.allHouses.length) {
+      const randomIndex = Math.floor(Math.random() * this.allHouses.length);
+      const featuredHouse = this.allHouses[randomIndex];
+      this.setState({ featuredHouse })
     };
-    fetchHouses();
-  }, []);
+  }
 
-  let determineFeaturedHouse = () => {
-  if (allHouses.length) {
-    const randomIndex = Math.floor(Math.random() * allHouses.length);
-    const featuredHouse = allHouses[randomIndex];
-    this.setState({ featuredHouse })
-  };
-}
+  determineUniqueConstries = () => {
+    const countries = this.allHouses 
+    ? Array.from(new Set(this.allHouses.map(h=> h.country))) 
+    : [];
+    countries.unshift(null);
+    this.setState({ countries });
+  }
 
-  return (
-    <div className="container">
-      <Header subtitle="Providing houses all over the world" />
+
+
+  render() {
+    if(this.state.hasError) {
+      return <h1> Whoops! Sorry! </h1>;
+    }
+    return ( 
+      <div className = "container" >
+      <Header subtitle = "Providing houses all over the world" />
+      <FeatureHouse house={this.state.featuredHouse}/>
+      <HouseFilter house={this.state.featuredHouse}/>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
