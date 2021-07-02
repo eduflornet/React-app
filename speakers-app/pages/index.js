c194790d02ca45fc14fe2fef6f3254d197d03be7
Eduardo Flores <@eduflornet>
2021-07-02 17:22:16 +0200

    Added refactoring about components: Header, Speaker, Speakers, SpeakersList and SpeakersToolbar.

--- speakers-app/pages/index.js
+++ speakers-app/pages/index.js
@@ -1,24 +1,6 @@
-import { data } from "../SpeakerData";
-import Speaker from "../src/components/Speakers/Speakers";
-
+import Speakers from "../src/components/Speakers";
 const IndexPage = () => {
-
-  
-
-  return (
-    <div className="container speakers-list">
-      <div className="row">
-      
-        
-        {data.map(function (speaker) {
-          if(speaker){
-            return <Speaker key={speaker?.id} speaker={speaker} />;
-          }
-          
-        })}
-      </div>
-    </div>
-  );
+  return <Speakers />;
 };
 
 export default IndexPage;
--- speakers-app/pages/speakers.js
+++ speakers-app/pages/speakers.js
@@ -1,7 +1,7 @@
 import Header from '../src/components/Header/Header';
 import Menu from '../src/components/Menu/Menu';
 import SpeakerSearchBar from '../src/components/SpeakerSearchBar/SpeakerSearchBar';
-import Speakers from '../src/components/Speakers/Speakers';
+import Speakers from '../src/components/Speakers';
 import Footer from '../src/components/Footer/Footer';
 
 function Page(){
