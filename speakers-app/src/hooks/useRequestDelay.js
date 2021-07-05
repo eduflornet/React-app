e37291ebedf0d4c0d2ed417d2ec712446b80ae68
Eduardo Flores <@eduflornet>
2021-07-05 15:36:30 +0200

    Building and using a dedicated speaker context. Adding insert and Delete to our request context's update capability. Implemented a React UI to create and delete speakers.

--- speakers-app/src/hooks/useRequestDelay.js
+++ speakers-app/src/hooks/useRequestDelay.js
@@ -30,10 +30,60 @@ function useRequestDelay(delayTime = 1000, initialData=[]) {
     delayFunc();
   }, []);
 
-  function updateRecord(recordUpdated, doneCallback) {
+  function deleteRecord(record, doneCallback) {
+    const originalRecords = [...data];
+    const newRecords = data.filter(function(rec){
+      return rec.id != record.id;
+    });
+
+    async function delayFunction() {
+      try {
+        setData(newRecords);
+        await delay(delayTime);
+        if(doneCallback){
+          doneCallback();
+        }
+        
+      } catch (error) {
+        console.log("error thrown inside delayFunction", error);
+        if(doneCallback){
+          doneCallback();
+        }
+        setData(originalRecords);
+      }
+    }
+    delayFunction();
+
+  }
+
+  function insertRecord(record, doneCallback) {
+    const originalRecords = [...data];
+    const newRecords = [record, ...data];
+
+    async function delayFunction() {
+      try {
+        setData(newRecords);
+        await delay(delayTime);
+        if(doneCallback){
+          doneCallback();
+        }
+        
+      } catch (error) {
+        console.log("error thrown inside delayFunction", error);
+        if(doneCallback){
+          doneCallback();
+        }
+        setData(originalRecords);
+      }
+    }
+    delayFunction();
+
+  }
+
+  function updateRecord(record, doneCallback) {
     const originalRecords = [...data];
     const newRecords = data.map(function (rec) {
-      return rec.id === recordUpdated.id ? recordUpdated : rec;
+      return rec.id === record.id ? record : rec;
     })
 
     async function delayFunction() {
@@ -61,6 +111,8 @@ function useRequestDelay(delayTime = 1000, initialData=[]) {
     requestStatus,
     error,
     updateRecord,
+    insertRecord,
+    deleteRecord
   };
 }
 
